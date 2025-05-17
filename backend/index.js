1c70631da6505c3f5ae1cc09543ed8b5414e7b19
Bui_Trung_Kien <kienbui27032004@gmail.com>
2025-05-17 16:18:59 +0700

    Add backend Node.js server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// backend/index.js
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Vietnam is Awesome - Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
